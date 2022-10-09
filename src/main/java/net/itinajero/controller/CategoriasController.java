b646a672441639052c6c9d826809eae2a9deabe3
AddyHerrera <addy.herrerapaz@cbtis72.edu.mx>
2022-10-09 02:14:25 -0500

    Subi las modificaciones del video 4 al 6
    Realice las modificaciones correspondientes a los videos 4,5 y 6, donde se agrega: @Primary y activamos lo de categoria.

--- src/main/java/net/itinajero/controller/CategoriasController.java
+++ src/main/java/net/itinajero/controller/CategoriasController.java
@@ -1,25 +1,23 @@
 package net.itinajero.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import net.itinajero.model.Categoria;
 import net.itinajero.service.ICategoriasService;
-
 
 @Controller
 @RequestMapping(value="/categorias")
 public class CategoriasController {
 	
-	
 	@Autowired
+	//@Qualifier("categoriasServiceJpa")
    	private ICategoriasService serviceCategorias;
 	
 	@RequestMapping(value="/index", method=RequestMethod.GET)
