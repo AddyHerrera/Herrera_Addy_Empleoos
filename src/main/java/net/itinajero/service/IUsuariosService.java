7e7156f5a944572ccf9ff71912421edf778e20d4
AddyHerrera <addy.herrerapaz@cbtis72.edu.mx>
2022-10-12 15:11:47 -0500

    Subo las modificaciones que realice en los video 4,5 y 6
    En el video 4 recuperamos los roles del usuario que inició sesión en un controlador, donde la modificación se encuentra en homecontroller (linea 104), en el video 5 se agregó el HttpSession que sirve para  agregar datos en la sesión del usuario. En el video 6 se empezó a ver Expresiones comunes de Thymeleaf, el primero fue renderizar atributos almacenados en la sesión.

--- src/main/java/net/itinajero/service/IUsuariosService.java
+++ src/main/java/net/itinajero/service/IUsuariosService.java
@@ -8,10 +8,9 @@ import net.itinajero.model.Usuario;
 public interface IUsuariosService {
 	
 	void guardar(Usuario usuario);
-	
 	// Ejercicio: Método que elimina un usuario de la base de datos.
 	void eliminar(Integer idUsuario);
-		
 	// Ejercicio: Implementar método que recupera todos los usuarios. Usar vista de listUsuarios.html
 	List<Usuario> buscarTodos();
+	Usuario buscarPorUsername(String username);
 }
--- src/main/java/net/itinajero/service/db/UsuariosServiceJpa.java
+++ src/main/java/net/itinajero/service/db/UsuariosServiceJpa.java
@@ -1,11 +1,9 @@
 package net.itinajero.service.db;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
-
 import net.itinajero.model.Usuario;
 import net.itinajero.repository.UsuariosRepository;
 import net.itinajero.service.IUsuariosService;
@@ -31,6 +29,11 @@ public class UsuariosServiceJpa implements IUsuariosService {
 	@Override
 	public List<Usuario> buscarTodos() {
 		return usuariosRepo.findAll();
+	}
+	
+	@Override
+	public Usuario buscarPorUsername(String username) {
+		return usuariosRepo.findByUsername(username);
 	}
 
 }
